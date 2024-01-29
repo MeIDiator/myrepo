e83baecd010cd0e5e7446a954621ce7ca6bd462b
aaashthetic <116004738+aaashthetic@users.noreply.github.com>
2024-01-29 20:52:55 +0800

    First Commit

---
+++ src/main/java/Forms/MeIDiator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Forms;
+
+/**
+ *
+ * @author user
+ */
+public class MeIDiator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
